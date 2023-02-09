4a2eb0d8526f84be582c6b289f37ef2199773ea3
= <=>
2023-02-10 01:19:07 +0500

    updated db

--- db.sql
+++ db.sql
@@ -1,27 +1,3 @@
--- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
---
--- Host: localhost    Database: primeatle
--- ------------------------------------------------------
--- Server version	8.0.28
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `account`
---
-
-DROP TABLE IF EXISTS `account`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `account` (
   `id` int NOT NULL AUTO_INCREMENT,
   `fullName` varchar(500) DEFAULT NULL,
@@ -29,25 +5,16 @@ CREATE TABLE `account` (
   `password` varchar(1000) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `account`
---
 
 LOCK TABLES `account` WRITE;
-/*!40000 ALTER TABLE `account` DISABLE KEYS */;
 INSERT INTO `account` VALUES (1,NULL,'waqas@email.com','$2b$10$ZYfz3X7IO5WdcUqw/5F7M.VlNtXrV7O5O9vxBZ10xrT10c3BWGJNq'),(2,NULL,'adf@sdfasd','$2b$10$ch8Pf7.738qM8ya5PrWuUOE/h1.cwkSLPV..e.UXA/3BuYx1wbVr2');
-/*!40000 ALTER TABLE `account` ENABLE KEYS */;
+
 UNLOCK TABLES;
 
---
--- Table structure for table `customer`
---
+
 
 DROP TABLE IF EXISTS `customer`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `customer` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(500) DEFAULT NULL,
@@ -56,23 +23,34 @@ CREATE TABLE `customer` (
   `note` varchar(5000) DEFAULT NULL,
   `todaysPickup` varchar(500) DEFAULT NULL,
   `meals` varchar(500) DEFAULT NULL,
+  `userid` int DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Dumping data for table `customer`
---
+LOCK TABLES `customer` WRITE;INSERT INTO `customer` VALUES (1,'Aakash','aakash@email.com','+91254546','adsfladskjfkdashjkfhasdkjfsadl','10','5',1),(2,'Salman','asdf@sadfas','asdfasd','asdf','10','10',1),(3,'dasfkasdhfh','dashfkjlah@dasjkflk','46546546','asdhfkjlads','10','10',NULL);
 
-LOCK TABLES `customer` WRITE;
-/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
-INSERT INTO `customer` VALUES (1,'Aakash','aakash@email.com','+91254546','adsfladskjfkdashjkfhasdkjfsadl','10','5'),(2,'asdf','asdf@sadfas','asdfasd','asdf','10','10');
-/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
 UNLOCK TABLES;
 
 
+DROP TABLE IF EXISTS `pickups`;
+
+CREATE TABLE `pickups` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `customerid` int DEFAULT NULL,
+  `pickup` int DEFAULT NULL,
+  `enrtydate` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+LOCK TABLES `pickups` WRITE;
+/*!40000 ALTER TABLE `pickups` DISABLE KEYS */;
+INSERT INTO `pickups` VALUES (1,1,3,'2023-02-09 17:54:00'),(2,1,4,'2023-02-09 22:54:00');
+/*!40000 ALTER TABLE `pickups` ENABLE KEYS */;
+UNLOCK TABLES;
+
 DELIMITER ;;
-CREATE PROCEDURE `sp_create_account`(
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_create_account`(
 in _email varchar(500),
 in _password varchar(1000)
 )
@@ -83,35 +61,105 @@ END ;;
 DELIMITER ;
 
 DELIMITER ;;
-CREATE  PROCEDURE `sp_create_customer`(
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_create_customer`(
 in _name varchar(500),
 in _email varchar(500),
 in _phone varchar(500),
 in _note varchar(5000),
 in _todaysPickup varchar(500),
-in _meals varchar(500)
+in _meals varchar(500),
+in _userid int
 
 )
 BEGIN
-INSERT INTO `customer`(`name`,`email`,`phone`,`note`,`todaysPickup`,`meals`)
-VALUES(_name,_email,_phone,_note,_todaysPickup,_meals);
+INSERT INTO `customer`(`name`,`email`,`phone`,`note`,`todaysPickup`,`meals`,`userid`)
+VALUES(_name,_email,_phone,_note,_todaysPickup,_meals,_userid);
 select * from `customer` where id =last_insert_id();
 
 END ;;
 DELIMITER ;
 
 DELIMITER ;;
-CREATE PROCEDURE `sp_get_customers`()
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_create_pickup`(
+in _customerid int,
+in _number int
+)
 BEGIN
-select * from `customer`;
+INSERT INTO `pickups`
+(`customerid`,`pickup`,`enrtydate`)VALUES
+(_customerid,_number,now());
+select * from  `pickups` where customerid =_customerid;
 END ;;
 DELIMITER ;
 
 DELIMITER ;;
-CREATE PROCEDURE `sp_get_user_by_email`(
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_get_customers`(
+in _userid int
+)
+BEGIN
+select * from `customer` where userid = _userid;
+END ;;
+DELIMITER ;
+
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_get_pickups`(
+in _cid int
+)
+BEGIN
+select * from pickups where customerid = _cid;
+END ;;
+DELIMITER ;
+
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_get_user_by_email`(
 in _email varchar(500)
 )
 BEGIN
 select * from `account` where email = _email;
 END ;;
 DELIMITER ;
+;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_update_customer`(
+in _name varchar(500),
+in _email varchar(500),
+in _phone varchar(500),
+in _note varchar(5000),
+in _todaysPickup varchar(500),
+in _meals varchar(500),
+in _id int
+
+)
+BEGIN
+UPDATE `customer`
+SET
+`name` = _name,
+`email` = _email,
+`phone` = _phone,
+`note` = _note,
+`todaysPickup` = _todaysPickup,
+`meals` = _meals
+WHERE `id` = _id;
+select * from `customer` where id = _id;
+END ;;
+DELIMITER ;
+
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_update_pickups`(
+in _id int,
+in _cid int,
+in _num int,
+in _date datetime
+
+)
+BEGIN
+UPDATE `pickups`
+SET
+`customerid` = _cid,
+`pickup` = _num,
+`enrtydate` = _date
+WHERE `id` = _id;
+select * from  `pickups` where customerid =_cid;
+END ;;
+DELIMITER ;
+
